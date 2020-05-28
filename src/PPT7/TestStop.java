5ffa42ba1dccb943e9438e78eb34e919e52aca88
kuki-xie <xieronghaii@outlook.com>
2020-05-28 23:56:17 +0800

    3 submit

--- src/PPT7/TestStop.java
+++ src/PPT7/TestStop.java
@@ -2,6 +2,7 @@ package PPT7;
 
 public class TestStop {
     public static void main(String[] args){
+        long x=System.currentTimeMillis();
         Tick t=new Tick();
         new Thread(t).start();
         try{
@@ -9,11 +10,13 @@ public class TestStop {
         } catch (Exception e){
 
         }
-        System.out.println("quiting Task...");
+        System.out.println("quiting Task...   "+x);
         t.stopRunning();
     }
 }
 class Tick implements Runnable{
+    static long zeroTime;
+    long x=System.currentTimeMillis();
     private boolean timeToQuit=false;
     public void stopRunning(){
         timeToQuit=true;
@@ -22,12 +25,12 @@ class Tick implements Runnable{
         while (!timeToQuit){
             try {
                 Thread.sleep(1000);
-                System.out.println("tick...");
+                System.out.println("tick...   "+x);
             } catch (Exception e){
 
             }
         }
-        System.out.println("Tick finished.");
+        System.out.println("Tick finished.   "+x);
     }
 
 }
